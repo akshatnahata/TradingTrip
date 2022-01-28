9e5770d0fb62d872e5087635b78e449bbdc2ce37
akshatnahata <85619218+akshatnahata@users.noreply.github.com>
2022-01-28 23:27:16 +0530

    flask

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+app.secret_key = 'key'
+
 
 @app.route('/')
+
 def home():
    return render_template('index.html')
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
